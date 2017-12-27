6b368319548cae98332753ae7f6cea35873e450a
qiaoshi.wang <qiaoshi.wang@tongdun.cn>
2017-12-27 15:54:44 +0800

    添加迭代器例子

--- src/main/java/book/action/IteratorL.scala
+++ src/main/java/book/action/IteratorL.scala
@@ -7,7 +7,17 @@ object IteratorL {
 
   def main(args: Array[String]) {
 
+    val it =  Iterator[String]("name1","name2","name3")
 
+    def rename(name:String): String  ={
+      println(name)
+      name+"1"
+    }
+
+    val it2  = it.map(name=>rename(name))
+    it2.foreach(name=>{
+      println(name)
+    })
 
   }
 
